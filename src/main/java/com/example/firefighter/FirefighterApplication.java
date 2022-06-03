9dc069720f34c02d7bdbd12d3a88292cb714d171
maked0nski <maked0nskihtc@gmail.com>
2022-06-03 12:36:51 +0300

    Initial commit

---
+++ src/main/java/com/example/firefighter/FirefighterApplication.java
@@ -0,0 +1,13 @@
+package com.example.firefighter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirefighterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirefighterApplication.class, args);
+	}
+
+}
